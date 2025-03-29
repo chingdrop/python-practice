b75e038cadc60a5befd5419778f9f16e0979e159
Craig Hurley <chingdorp@pm.me>
2025-03-29 16:15:21 -0400

    black formatting

--- programming_concepts/futval.py
+++ programming_concepts/futval.py
@@ -2,6 +2,7 @@
 # Latest Version - Chapter 3
 # A program to compute the value of an investment carried 10 years into the future
 
+
 def main():
     print("This program calculates the future value")
     print("of a 10-year investment.")
@@ -13,5 +14,6 @@ def main():
         principal = principal * (1 + apr)
 
     print("The value in 10 years is:", principal)
+
 
 main()
