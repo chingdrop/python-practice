ada2d23d0f41d6d6e9f27e20f6ca33b774ae10f6
Craig Hurley <chingdorp@pm.me>
2025-03-29 16:06:02 -0400

    black formatting

--- programming_concepts/convert.py
+++ programming_concepts/convert.py
@@ -4,9 +4,10 @@
 # A program to convert Celsius temps to Fahrenheit.
 # This version issues heat and cold warnings.
 
+
 def main():
     celsius = float(input("What is the Celsius temperature? "))
-    fahrenheit = 9/5 * celsius + 32
+    fahrenheit = 9 / 5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
 
     # Print warnings for extreme temps
@@ -14,5 +15,6 @@ def main():
         print("It's really hot out there. Be careful!")
     if fahrenheit < 30:
         print("Brrrrr. Be sure to dress warmly!")
+
 
 main()
