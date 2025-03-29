a55cf8fe2ece9478a63119e8d006e9ed3466a3fa
Craig Hurley <chingdorp@pm.me>
2025-03-29 14:49:26 -0400

    chapter 2 future value function

---
+++ futval.py
@@ -0,0 +1,13 @@
+def futval():
+    print("This program calculates the future value of a 10-year investment.")
+
+    principal = eval(input("Enter the initial principal: "))
+    apr = eval(input("Enter the annual interest rate: "))
+
+    for i in range(10):
+        principal = principal * (1 + apr)
+
+    print("The value in 10 years is:", principal)
+
+if __name__ == "__main__":
+    futval()
