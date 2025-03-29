3da33484ce9c0581900a5cf6fb47c849b531c4c9
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:16:29 -0400

    black formatting

--- programming_concepts/numbers2text.py
+++ programming_concepts/numbers2text.py
@@ -1,23 +1,25 @@
 # File - numbers2text.py
 # Version 2
 # Latest Version - Chapter 5
-# A program to convert a sequence of Unicode numbers into a string of text. 
+# A program to convert a sequence of Unicode numbers into a string of text.
 # Efficient version using a list accumulator.
+
 
 def main():
     print("This program converts a sequence of Unicode numbers into")
     print("the string of text that it represents.\n")
-    
+
     # Get the message to encode
     inString = input("Please enter the Unicode-encoded message: ")
 
     # Loop through each substring and build Unicode message
-    chars = [] 
+    chars = []
     for numStr in inString.split():
-        codeNum = int(numStr)              # convert digits to a number
-        chars.append(chr(codeNum))         # accumulate new character
+        codeNum = int(numStr)  # convert digits to a number
+        chars.append(chr(codeNum))  # accumulate new character
 
     message = "".join(chars)
     print("\nThe decoded message is:", message)
+
 
 main()
