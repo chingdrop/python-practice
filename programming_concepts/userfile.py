e3b0f758ea43783a86ac6ef11eecc905ffa84908
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:37:12 -0400

    fix print strings and add context managers

--- programming_concepts/userfile.py
+++ programming_concepts/userfile.py
@@ -4,31 +4,29 @@
 
 
 def main():
-    print("This program creates a file of usernames from a")
-    print("file of names.")
+    print("This program creates a file of usernames from a file of names.")
 
     # get the file names
-    infileName = input("What file are the names in? ")
-    outfileName = input("What file should the usernames go in? ")
-
-    # open the files
-    infile = open(infileName, "r")
-    outfile = open(outfileName, "w")
+    input_file_path = input("What file are the names in? ")
+    output_file_path = input("What file should the usernames go in? ")
 
     # process each line of the input file
-    for line in infile:
-        # get the first and last names from line
-        first, last = line.split()
-        # create the username
-        uname = (first[0] + last[:7]).lower()
-        # write it to the output file
-        print(uname, file=outfile)
+    with open(input_file_path, "r") as input_file:
+        for line in input_file:
+            # get the first and last names from line
+            first, last = line.split()
+            # create the username
+            uname = (first[0] + last[:7]).lower()
+            # write it to the output file
+            with open(output_file_path, "w") as output_file:
+                print(uname, file=output_file)
 
     # close both files
-    infile.close()
-    outfile.close()
+    input_file.close()
+    output_file.close()
 
-    print("Usernames have been written to", outfileName)
+    print(f"Usernames have been written to {output_file_path}")
 
 
-main()
+if __name__ == "__main__":
+    main()
