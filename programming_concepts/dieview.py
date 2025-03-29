9c498d92d50c36cf2aa2f40c24af7fddae0e7ccd
Craig Hurley <chingdorp@pm.me>
2025-03-29 18:29:06 -0400

    improve dieview

--- programming_concepts/dieview.py
+++ programming_concepts/dieview.py
@@ -1,10 +1,10 @@
-# dieview.py
+# dieview2.py
 from graphics import *
 
 
 class DieView:
-    """DieView is a widget that displays a graphical representation
-    of a standard six-sided die."""
+    """DieView is a widget that displays a graphical
+    representation of a standard six-sided die."""
 
     def __init__(self, win, center, size):
         """Create a view of a die, e.g.:
@@ -13,11 +13,11 @@ class DieView:
         of length 20."""
 
         # first define some standard values
-        self.win = win  # save this for drawing pips later
+        self.win = win
         self.background = "white"  # color of die face
         self.foreground = "black"  # color of the pips
         self.psize = 0.1 * size  # radius of each pip
-        hsize = size / 2.0  # half the size of the die
+        hsize = size / 2.0  # half of size
         offset = 0.6 * hsize  # distance from center to outer pips
 
         # create a square for the face
@@ -29,19 +29,31 @@ class DieView:
         rect.setFill(self.background)
 
         # Create 7 circles for standard pip locations
-        self.pip1 = self.__makePip(cx - offset, cy - offset)
-        self.pip2 = self.__makePip(cx - offset, cy)
-        self.pip3 = self.__makePip(cx - offset, cy + offset)
-        self.pip4 = self.__makePip(cx, cy)
-        self.pip5 = self.__makePip(cx + offset, cy - offset)
-        self.pip6 = self.__makePip(cx + offset, cy)
-        self.pip7 = self.__makePip(cx + offset, cy + offset)
+        self.pips = [
+            self.__makePip(cx - offset, cy - offset),
+            self.__makePip(cx - offset, cy),
+            self.__makePip(cx - offset, cy + offset),
+            self.__makePip(cx, cy),
+            self.__makePip(cx + offset, cy - offset),
+            self.__makePip(cx + offset, cy),
+            self.__makePip(cx + offset, cy + offset),
+        ]
 
-        # Draw an initial value
+        # Create a table for which pips are on for each value
+        self.onTable = [
+            [],
+            [3],
+            [2, 4],
+            [2, 3, 4],
+            [0, 2, 4, 6],
+            [0, 2, 3, 4, 6],
+            [0, 1, 2, 4, 5, 6],
+        ]
+
         self.setValue(1)
 
     def __makePip(self, x, y):
-        "Internal helper method to draw a pip at (x,y)"
+        """Internal helper method to draw a pip at (x,y)"""
         pip = Circle(Point(x, y), self.psize)
         pip.setFill(self.background)
         pip.setOutline(self.background)
@@ -49,41 +61,11 @@ class DieView:
         return pip
 
     def setValue(self, value):
-        "Set this die to display value."
-        # turn all pips off
-        self.pip1.setFill(self.background)
-        self.pip2.setFill(self.background)
-        self.pip3.setFill(self.background)
-        self.pip4.setFill(self.background)
-        self.pip5.setFill(self.background)
-        self.pip6.setFill(self.background)
-        self.pip7.setFill(self.background)
+        """Set this die to display value."""
+        # Turn all the pips off
+        for pip in self.pips:
+            pip.setFill(self.background)
 
-        # turn correct pips on
-        if value == 1:
-            self.pip4.setFill(self.foreground)
-        elif value == 2:
-            self.pip1.setFill(self.foreground)
-            self.pip7.setFill(self.foreground)
-        elif value == 3:
-            self.pip1.setFill(self.foreground)
-            self.pip7.setFill(self.foreground)
-            self.pip4.setFill(self.foreground)
-        elif value == 4:
-            self.pip1.setFill(self.foreground)
-            self.pip3.setFill(self.foreground)
-            self.pip5.setFill(self.foreground)
-            self.pip7.setFill(self.foreground)
-        elif value == 5:
-            self.pip1.setFill(self.foreground)
-            self.pip3.setFill(self.foreground)
-            self.pip4.setFill(self.foreground)
-            self.pip5.setFill(self.foreground)
-            self.pip7.setFill(self.foreground)
-        else:
-            self.pip1.setFill(self.foreground)
-            self.pip2.setFill(self.foreground)
-            self.pip3.setFill(self.foreground)
-            self.pip5.setFill(self.foreground)
-            self.pip6.setFill(self.foreground)
-            self.pip7.setFill(self.foreground)
+        # Turn the appropriate pips back on
+        for i in self.onTable[value]:
+            self.pips[i].setFill(self.foreground)
