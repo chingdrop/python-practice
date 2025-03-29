284152832fc98a2ec801543a3f9a2bef25af7232
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:59:44 -0400

    change var names and add context manager

--- programming_concepts/average.py
+++ programming_concepts/average.py
@@ -4,18 +4,19 @@
 
 
 def main():
-    fileName = input("What file are the numbers in? ")
-    infile = open(fileName, "r")
-    total = 0.0
-    count = 0
-    line = infile.readline()
-    while line != "":
-        # update total and count for values in line
-        for xStr in line.split(","):
-            total = total + float(xStr)
-            count = count + 1
-        line = infile.readline()
-    print("\nThe average of the numbers is", total / count)
+    file_path = input("What file are the numbers in? ")
+    with open(file_path, "r") as input_file:
+        total = 0.0
+        count = 0
+        line = input_file.readline()
+        while line != "":
+            # update total and count for values in line
+            for x_str in line.split(","):
+                total = total + float(x_str)
+                count = count + 1
+            line = input_file.readline()
+        print(f"\nThe average of the numbers is {total / count}")
 
 
-main()
+if __name__ == "__main__":
+    main()
