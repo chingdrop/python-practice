df6e5e392158552fe7147b9911e00ed1008d625c
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:18:37 -0400

    my proposed changes

--- programming_concepts/numbers2text.py
+++ programming_concepts/numbers2text.py
@@ -6,20 +6,22 @@
 
 
 def main():
-    print("This program converts a sequence of Unicode numbers into")
-    print("the string of text that it represents.\n")
+    print(
+        "This program converts a sequence of Unicode numbers into the string of text that it represents.\n"
+    )
 
     # Get the message to encode
-    inString = input("Please enter the Unicode-encoded message: ")
+    input_str = input("Please enter the Unicode-encoded message: ")
 
     # Loop through each substring and build Unicode message
     chars = []
-    for numStr in inString.split():
-        codeNum = int(numStr)  # convert digits to a number
-        chars.append(chr(codeNum))  # accumulate new character
+    for num_str in input_str.split():
+        code_num = int(num_str)  # convert digits to a number
+        chars.append(chr(code_num))  # accumulate new character
 
     message = "".join(chars)
-    print("\nThe decoded message is:", message)
+    print(f"\nThe decoded message is: {message}")
 
 
-main()
+if __name__ == "__main__":
+    main()
