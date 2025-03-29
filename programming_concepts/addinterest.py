91eeb05bcdcedb019892348b8f6c6ad47ee183cb
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:44:53 -0400

    add final version of addinterest module

---
+++ programming_concepts/addinterest.py
@@ -0,0 +1,15 @@
+# File - addinterest.py
+# Version 3
+# Latest Version - Chapter 6
+
+def addInterest(balances, rate):
+    for i in range(len(balances)):
+        balances[i] = balances[i] * (1+rate)
+
+def test():
+    amounts = [1000, 2200, 800, 360]
+    rate = 0.05
+    addInterest(amounts, rate)
+    print(amounts)
+
+test()
