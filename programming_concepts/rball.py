6ad2d98a06eaebba70430958789e3278edf1642d
Craig Hurley <chingdorp@pm.me>
2025-03-29 18:03:36 -0400

    black formatting

--- programming_concepts/rball.py
+++ programming_concepts/rball.py
@@ -4,11 +4,13 @@
 
 from random import random
 
+
 def main():
     printIntro()
     probA, probB, n = getInputs()
     winsA, winsB = simNGames(n, probA, probB)
     printSummary(winsA, winsB)
+
 
 def printIntro():
     print("This program simulates a game of racquetball between two")
@@ -17,12 +19,14 @@ def printIntro():
     print("the player wins the point when serving. Player A always")
     print("has the first serve.")
 
+
 def getInputs():
     # Returns the three simulation parameters
     a = float(input("What is the prob. player A wins a serve? "))
     b = float(input("What is the prob. player B wins a serve? "))
     n = int(input("How many games to simulate? "))
     return a, b, n
+
 
 def simNGames(n, probA, probB):
     # Simulates n games of racquetball between players whose
@@ -36,6 +40,7 @@ def simNGames(n, probA, probB):
         else:
             winsB = winsB + 1
     return winsA, winsB
+
 
 def simOneGame(probA, probB):
     # Simulates a single game or racquetball between players whose
@@ -57,16 +62,20 @@ def simOneGame(probA, probB):
                 serving = "A"
     return scoreA, scoreB
 
+
 def gameOver(a, b):
     # a and b represent scores for a racquetball game
     # Returns True if the game is over, False otherwise.
-    return a==15 or b==15
+    return a == 15 or b == 15
+
 
 def printSummary(winsA, winsB):
     # Prints a summary of wins for each player.
     n = winsA + winsB
     print("\nGames simulated:", n)
-    print("Wins for A: {0} ({1:0.1%})".format(winsA, winsA/n))
-    print("Wins for B: {0} ({1:0.1%})".format(winsB, winsB/n))
+    print("Wins for A: {0} ({1:0.1%})".format(winsA, winsA / n))
+    print("Wins for B: {0} ({1:0.1%})".format(winsB, winsB / n))
 
-if __name__ == '__main__': main()
+
+if __name__ == "__main__":
+    main()
