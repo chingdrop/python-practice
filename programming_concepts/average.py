d7c32d001be224cacbb1077e81521dda84c471aa
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:55:40 -0400

    add final version of average module

---
+++ programming_concepts/average.py
@@ -0,0 +1,20 @@
+# average.py
+# Version 7
+# Latest Version - Chapter 8
+
+def main():
+    fileName = input("What file are the numbers in? ")
+    infile = open(fileName,'r')
+    total = 0.0
+    count = 0
+    line = infile.readline()
+    while line != "":
+        # update total and count for values in line
+        for xStr in line.split(","):
+            total = total + float(xStr)
+            count = count + 1
+        line = infile.readline()
+    print("\nThe average of the numbers is", total / count)
+
+main()
+
