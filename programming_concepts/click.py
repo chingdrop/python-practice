41e532f40076bd993abcd5236b3a672db9081de6
Craig Hurley <chingdorp@pm.me>
2025-03-29 16:44:43 -0400

    black formatting

--- programming_concepts/click.py
+++ programming_concepts/click.py
@@ -2,10 +2,12 @@
 # Latest Version - Chapter 4
 from graphics import *
 
+
 def main():
     win = GraphWin("Click Me!")
     for i in range(10):
         p = win.getMouse()
         print("You clicked at:", p.getX(), p.getY())
+
 
 main()
