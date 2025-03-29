034c61ee68b4446afa5a31407b092f81a68e9b5e
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:23:54 -0400

    add final version of printfile module

---
+++ programming_concepts/printfile.py
@@ -0,0 +1,11 @@
+# File - printfile.py
+# Latest Version - Chapter 5
+# Prints a file to the screen.
+
+def main():
+    fname = input("Enter filename: ")
+    infile = open(fname,"r")
+    data = infile.read()
+    print(data)
+
+main()
