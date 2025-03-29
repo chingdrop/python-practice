10b1a762900c6589de63294aef7afb2b9480eafb
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:47:34 -0400

    black formatting

--- programming_concepts/happy.py
+++ programming_concepts/happy.py
@@ -3,15 +3,19 @@
 # Latest Version - Chapter 6
 # Happy Birthday using value returning functions
 
+
 def happy():
     return "Happy birthday to you!\n"
 
+
 def verseFor(person):
-    lyrics = happy()*2 + "Happy birthday, dear " + person + ".\n" + happy()
+    lyrics = happy() * 2 + "Happy birthday, dear " + person + ".\n" + happy()
     return lyrics
+
 
 def main():
     for person in ["Fred", "Lucy", "Elmer"]:
         print(verseFor(person))
+
 
 main()
