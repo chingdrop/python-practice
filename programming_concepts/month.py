195c4f93e2e39779a966f5ccf4bca761fd8cbd3c
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:11:38 -0400

    add final version of month module

---
+++ programming_concepts/month.py
@@ -0,0 +1,16 @@
+# File - month.py
+# Version 2
+# Latest Version - Chapter 5
+# A program to print the month abbreviation, given its number.
+
+def main():
+    # months is a list used as a lookup table
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "  Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    n = int(input("Enter a month number (1-12): "))
+
+    print("The month abbreviation is", months[n-1] + ".")
+
+
+main()
