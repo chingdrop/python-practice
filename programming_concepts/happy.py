945ee246d116f5d75b7fcd4093cc5ca6778815ec
Craig Hurley <chingdorp@pm.me>
2025-03-29 17:47:21 -0400

    add final version of happy module

---
+++ programming_concepts/happy.py
@@ -0,0 +1,17 @@
+# File - happy.py
+# Version 2
+# Latest Version - Chapter 6
+# Happy Birthday using value returning functions
+
+def happy():
+    return "Happy birthday to you!\n"
+
+def verseFor(person):
+    lyrics = happy()*2 + "Happy birthday, dear " + person + ".\n" + happy()
+    return lyrics
+
+def main():
+    for person in ["Fred", "Lucy", "Elmer"]:
+        print(verseFor(person))
+
+main()
