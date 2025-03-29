a33f24be08222f388f81640990f98651be4bc3ab
Craig Hurley <chingdorp@pm.me>
2025-03-29 16:01:20 -0400

    my proposed changes

--- programming_concepts/chaos.py
+++ programming_concepts/chaos.py
@@ -4,10 +4,11 @@
 
 def main():
     print("This program illustrates a chaotic function")
-    x = eval(input("Enter a number between 0 and 1: "))
+    x = float(input("Enter a number between 0 and 1: "))
     for i in range(10):
         x = 3.9 * x * (1 - x)
         print(x)
 
 
-main()
+if __name__ == "__main__":
+    main()
