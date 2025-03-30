659ffd4e88eeb1242e35f730aa9b43a4fd3ec1f3
Craig Hurley <chingdorp@pm.me>
2025-03-29 22:09:17 -0400

    move all conversion funcs to the convert module

--- programming_concepts/convert.py
+++ programming_concepts/convert.py
@@ -56,3 +56,81 @@ def connect_gui():
     # wait for click and then quit
     win.getMouse()
     win.close()
+
+
+# Version 2
+# Latest Version - Chapter 5
+# Converts day month and year numbers into two date formats
+
+
+def date_convert():
+    # get the day month and year as numbers
+    day = int(input("Enter the day number: "))
+    month = int(input("Enter the month number: "))
+    year = int(input("Enter the year: "))
+
+    date1 = f"{str(month)} / {str(day) / {str(year)}}"
+
+    months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+    month_str = months[month - 1]
+    date2 = f"{month_str} {str(day)}, {str(year)}"
+
+    print(f"The date is: {date1} or {date2}.")
+
+
+# Version 2
+# Latest Version - Chapter 5
+# A program to convert a sequence of Unicode numbers into a string of text.
+# Efficient version using a list accumulator.
+
+
+def int_to_str():
+    print(
+        "This program converts a sequence of Unicode numbers into the string of text that it represents.\n"
+    )
+
+    # Get the message to encode
+    input_str = input("Please enter the Unicode-encoded message: ")
+
+    # Loop through each substring and build Unicode message
+    chars = []
+    for num_str in input_str.split():
+        code_num = int(num_str)  # convert digits to a number
+        chars.append(chr(code_num))  # accumulate new character
+
+    message = "".join(chars)
+    print(f"\nThe decoded message is: {message}")
+
+
+# Latest Version - Chapter 5
+# A program to convert a textual message into a sequence of numbers, utilizing the underlying Unicode encoding.
+
+
+def str_to_int():
+    print(
+        "This program converts a textual message into a sequence of numbers representing the Unicode encoding of the message.\n"
+    )
+
+    # Get the message to encode
+    message = input("Please enter the message to encode: ")
+
+    print("\nHere are the Unicode codes:")
+
+    # Loop through the message and print out the Unicode values
+    for ch in message:
+        print(ord(ch), end=" ")
+
+    print()  # End line of output
