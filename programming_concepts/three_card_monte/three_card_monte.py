58f1b5eba2809e48fab9af743a260ce5e0e3cbd1
Craig Hurley <chingdorp@pm.me>
2025-03-29 21:36:29 -0400

    refactor all three card monte funcs into same module

--- programming_concepts/three_card_monte/three_card_monte.py
+++ programming_concepts/three_card_monte/three_card_monte.py
@@ -3,13 +3,123 @@
 # threecardmonte.py
 
 from random import randrange
-from programming_concepts.classes.graphics import GraphWin, Point
+from programming_concepts.classes.graphics import Circle, GraphWin, Line, Point, Text
 from programming_concepts.classes.button import Button
-from programming_concepts.three_card_monte.outcome_view import OutcomeView
-from programming_concepts.three_card_monte.game_text import GameText
+
+class GameText:
+    """A custom wigdet that displays text. The text can be changed by its value.
+    The text will isplay the win, lose, welcoome and quit messages."""
+
+    def __init__(self, win, center):
+        """Creates Interactive Text, e.g.:
+           message = GameText(myWin, Point(40,50))
+        creates interactive text centered at (40,50)"""
+
+        # initializes are parameters used by the methods.
+        self.win = win
+        cx, cy = center.getX(), center.getY()
+        self.welcome = "Welcome to Three Card Monte"
+        self.win_message = "You win! Try again or quit."
+        self.lose_message = "You lose! Try again or quit."
+        self.w = "0"
+        self.l = "0"
+
+        # Constructs interactive text.
+        self.message1 = self.__make_text(cx, cy)
+
+    def __make_text(self, x, y):
+        "helper method that creates the interactive text."
+        game_message = Text(Point(x, y), "")
+        game_message.setText("Welcome to Three Card Monte")
+        game_message.draw(self.win)
+        return game_message
+
+    def set_count(self, win_count, lose_count):
+        "A mutator method that updates the win/lose method."
+        self.w = str(win_count)
+        self.l = str(lose_count)
+
+    def set_value(self, value):
+        "Sets the value that indicates which message to display."
+        # default value
+        self.message1.setText("")
+
+        # changes game text based on the outcome of the game.
+        if value == 1:
+            self.message1.setText(self.welcome)
+        elif value == 2:
+            self.message1.setText(self.win_message)
+        elif value == 3:
+            self.message1.setText(self.lose_message)
+
+        # Puts together the quit message.
+        else:
+            self.message1.setText(
+                f"You won {self.w} times and lost {self.l} times. Click anywhere to close."
+            )
 
 
-def main():
+class OutcomeView:
+    """Outcome View is a widget that creates a
+    visual interpretation of a win and a lose."""
+
+    def __init__(self, win, center, size):
+        """Create a circle for win or an X for lose, e.g.:
+           outcome1 = OutcomeView(myWin, Point(40,50), 20)
+        creates a circle and an ex centered at (40, 50)
+        and with the size of 20."""
+
+        # Constructs the parameters: colors, dimensions.
+        self.win = win
+        self.background = "peachpuff"
+        self.correct = "green"
+        self.wrong = "red"
+        cx, cy = center.getX(), center.getY()
+        self.csize = size / 2.5
+        lsize = size / 2.0
+
+        # Create the correct or wrong symbols for the outcome of the game
+        self.correct1 = self.__make_correct(cx, cy)
+        self.wrong1 = self.__make_wrong(cx - lsize, cx + lsize, cy + lsize, cy - lsize)
+        self.wrong2 = self.__make_wrong(cx + lsize, cx - lsize, cy + lsize, cy - lsize)
+
+    def __make_correct(self, x, y):
+        "Creates a circle and sets the background to the same color as the card."
+        correct_outcome = Circle(Point(x, y), self.csize)
+        correct_outcome.setFill(self.background)
+        correct_outcome.setOutline(self.background)
+        correct_outcome.draw(self.win)
+        return correct_outcome
+
+    def __make_wrong(self, x1, x2, y1, y2):
+        "Creates an X and sets the background to the card."
+        p1 = Point(x1, y1)
+        p2 = Point(x2, y2)
+        wrong_outcome = Line(p1, p2)
+        wrong_outcome.draw(self.win)
+        wrong_outcome.setFill(self.background)
+        return wrong_outcome
+
+    def set_outcome(self, value):
+        "Set the value of the image to color in a green circle or a red X"
+        # defaults the images to the card color.
+        self.correct1.setFill(self.background)
+        self.wrong1.setFill(self.background)
+        self.wrong2.setFill(self.background)
+
+        # changes the image to the correct color.
+        if value == 1:
+            self.correct1.setFill(self.correct)
+        elif value == 2:
+            self.wrong1.setFill(self.wrong)
+            self.wrong2.setFill(self.wrong)
+        else:
+            self.correct1.setFill(self.background)
+            self.wrong1.setFill(self.background)
+            self.wrong2.setFill(self.background)
+
+
+def three_card_monte():
 
     # Create the game view window
     win = GraphWin("GameView", 700, 500)
@@ -116,6 +226,3 @@ def main():
     message1.set_value(4)
     win.getMouse()
     win.close()
-
-
-main()
