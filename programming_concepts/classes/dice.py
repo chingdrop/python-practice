f9cc0c5ca2068a7dafc76804c468347f8cbee521
Craig Hurley <chingdorp@pm.me>
2025-03-29 22:35:13 -0400

    refactor msdie to dice module

--- programming_concepts/classes/dice.py
+++ programming_concepts/classes/dice.py
@@ -124,3 +124,19 @@ class ColorDieView(DieView):
     def set_color(self, color):
         self.foreground = color
         self.set_value(self.value)
+
+
+class MSDie:
+
+    def __init__(self, sides):
+        self.sides = sides
+        self.value = 1
+
+    def roll(self):
+        self.value = randrange(1, self.sides + 1)
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
