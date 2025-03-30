59ad951f50c882fbbb56539bd59ec8692e14fa4c
Craig Hurley <chingdorp@pm.me>
2025-03-29 21:16:06 -0400

    move gpa funcs into same module

--- programming_concepts/gpa/gpa.py
+++ programming_concepts/gpa/gpa.py
@@ -30,7 +30,7 @@ def make_student(info_str):
     return Student(name, hours, qpoints)
 
 
-def main():
+def gpa():
     # open the input file for reading
     filename = input("Enter name the grade file: ")
     with open(filename, "r") as input_file:
@@ -52,5 +52,27 @@ def main():
     )
 
 
-if __name__ == "__main__":
-    main()
+def read_students(filename):
+    with open(filename, "r") as input_file:
+        students = []
+        for line in input_file:
+            students.append(make_student(line))
+    return students
+
+
+def write_students(students, filename):
+    with open(filename, "w") as output_file:
+        for s in students:
+            print(
+                f"{s.get_name()}\t{s.get_hours()}\t{s.get_q_points()}", file=output_file
+            )
+
+
+def gpa_sort():
+    print("This program sorts student grade information by GPA")
+    filename = input("Enter the name of the data file: ")
+    data = read_students(filename)
+    data.sort(key=Student.gpa)
+    filename = input("Enter a name for the output file: ")
+    write_students(data, filename)
+    print(f"The data has been written to {filename}")
