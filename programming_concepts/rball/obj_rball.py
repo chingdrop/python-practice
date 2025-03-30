3f26443dfc48668edef89a73f34901d8a491a060
Craig Hurley <chingdorp@pm.me>
2025-03-29 22:28:31 -0400

    black formatting

--- programming_concepts/rball/obj_rball.py
+++ programming_concepts/rball/obj_rball.py
@@ -4,6 +4,7 @@
 
 from random import random
 
+
 class Player:
     # A Player keeps track of service probability and score
 
@@ -24,6 +25,7 @@ class Player:
         # Returns this player's current score
         return self.score
 
+
 class RBallGame:
     # A RBallGame represents a game in progress. A game has two players
     # and keeps track of which one is currently serving.
@@ -41,12 +43,11 @@ class RBallGame:
                 self.server.inc_score()
             else:
                 self.change_server()
-            
+
     def is_over(self):
         # Returns game is finished (i.e. one of the players has won).
-        a,b = self.get_scores()
-        return a == 15 or b == 15 or \
-               (a == 7 and b == 0) or (b==7 and a == 0)
+        a, b = self.get_scores()
+        return a == 15 or b == 15 or (a == 7 and b == 0) or (b == 7 and a == 0)
 
     def change_server(self):
         # Switch which player is serving
@@ -58,6 +59,7 @@ class RBallGame:
     def get_scores(self):
         # Returns the current scores of player A and player B
         return self.player_a.get_score(), self.player_b.get_score()
+
 
 class SimStats:
     # SimStats handles accumulation of statistics across multiple
@@ -74,11 +76,11 @@ class SimStats:
     def update(self, game):
         # Determine the outcome of aGame and update statistics
         a, b = game.getScores()
-        if a > b:                             # A won the game
+        if a > b:  # A won the game
             self.wins_a = self.wins_a + 1
             if b == 0:
                 self.shuts_a = self.shuts_a + 1
-        else:                                 # B won the game
+        else:  # B won the game
             self.wins_b = self.wins_b + 1
             if a == 0:
                 self.shuts_b = self.shuts_b + 1
@@ -86,19 +88,19 @@ class SimStats:
     def print_report(self):
         # Print a nicely formatted report
         n = self.wins_a + self.wins_b
-        print("Summary of", n , "games:\n")
+        print("Summary of", n, "games:\n")
         print("          wins (% total)   shutouts (% wins)  ")
         print("--------------------------------------------")
         self.print_line("A", self.wins_a, self.shuts_a, n)
         self.print_line("B", self.wins_b, self.shuts_b, n)
-            
+
     def print_line(self, label, wins, shuts, n):
         template = "Player {0}:{1:5}  ({2:5.1%}) {3:11}   ({4})"
-        if wins == 0:        # Avoid division by zero!
+        if wins == 0:  # Avoid division by zero!
             shutStr = "-----"
         else:
-            shutStr = "{0:4.1%}".format(float(shuts)/wins)
-        print(template.format(label, wins, float(wins)/n, shuts, shutStr)) 
+            shutStr = "{0:4.1%}".format(float(shuts) / wins)
+        print(template.format(label, wins, float(wins) / n, shuts, shutStr))
 
 
 def print_intro():
@@ -107,7 +109,8 @@ def print_intro():
     print("indicated by a probability (a number between 0 and 1) that")
     print("the player wins the point when serving. Player A always")
     print("has the first serve.\n")
-        
+
+
 def get_inputs():
     # Returns the three simulation parameters
     a = float(input("What is the prob. player A wins a serve? "))
@@ -115,20 +118,22 @@ def get_inputs():
     n = int(input("How many games to simulate? "))
     return a, b, n
 
+
 def obj_rball():
     print_intro()
-    
+
     probA, probB, n = get_inputs()
 
     # Play the games
     stats = SimStats()
     for i in range(n):
-        theGame = RBallGame(probA, probB) # create a new game
-        theGame.play()                    # play it
-        stats.update(theGame)             # extract info
+        theGame = RBallGame(probA, probB)  # create a new game
+        theGame.play()  # play it
+        stats.update(theGame)  # extract info
 
     # Print the results
     stats.print_report()
+
 
 obj_rball()
 input("\nPress <Enter> to quit")
